44c6a21bd542469b5a8912ddd74dd0267d460c64
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-27 08:38:35 -0700

    hwrk done

---
+++ W2D4/octopus.rb
@@ -0,0 +1,51 @@
+def sluggish_octopus(fishes)
+  
+  max = ""
+  fishes.each_index do |idx|
+    (idx + 1 ... fishes.length).each do |i|
+      if fishes[i].length > fishes[idx].length
+        max =fishes[i]
+      end 
+    end 
+  
+  end 
+  max
+end 
+
+def sort_octopuses(arr)
+  return arr if arr.length <= 1
+  mid = arr.length/2
+  left = sort_octopuses(arr.take(mid))
+  right = sort_octopuses(arr.drop(mid))
+  result = merge_octopuses(left,right)
+  
+end 
+
+def merge_octopuses(left,right)
+  result = []
+  until left.empty? || right.empty?
+    if left.first.length < right.first.length
+      result << left.shift
+    else 
+      result << right.shift
+    end 
+  end 
+  result + left + right
+end 
+
+def dominant_octopus(arr)
+  sort_octopuses(arr)[-1]
+end 
+
+def slow_dance(dir, arr)
+  arr.each_index do |idx|
+    if arr[idx] == dir 
+      return idx 
+    end 
+  end 
+end 
+
+def fast_dance(dir, hash_tiles)
+  return hash_tiles[dir]
+end 
+hash_tiles = {"up" : 0, "right-up": 1, "right": 2, "right-down" : 3, "down" : 4, "left-down" : 5, "left" : 6, "left-down": 7}
